c40c49b8cf76bb4e323879768d7c9eda4ec780e6
David Poslušný <david.poslusny@email.cz>
2023-04-08 14:35:14 +0200

    changes to modal with all events

--- src/components/ModalEvents.tsx
+++ src/components/ModalEvents.tsx
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 import { EventType } from "../../utils/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faLayerGroup } from "@fortawesome/free-solid-svg-icons";
 
 type ModalProps = {
   date: dayjs.Dayjs
@@ -22,31 +24,34 @@ const ModalEvents = ({ date, events, setShowModal, showModal }: ModalProps) => {
         className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50'
         onClick={clickOutside}
       >
-        <div className='relative w-auto my-6 mx-auto max-w-2xl'>
+        <div className='relative w-auto my-6 mx-auto max-w-2xl text-black'>
           <div 
             className='flex flex-col relative w-full rounded-xl shadow-2xl bg-white'
             onClick={e => e.stopPropagation()}
           >
             <div className='flex p-5 border-b-2 border-slate-200'>
-              <div className="flex flex-col justify-center items-center w-full">
-              <p className='text-2xl font-semibold'>
-                {date.locale('en').format('dddd').substring(0, 3)}
-              </p>
-              <p className='text-2xl font-semibold'>
-                {date.date()}
-              </p>
+              <div className='flex flex-col justify-center items-center w-full'>
+                <div className='flex justify-center items-center text-3xl mb-4 space-x-4'>
+                  <FontAwesomeIcon icon={faLayerGroup} />
+                  <p className='font-semibold'>
+                    All events
+                  </p>
+                </div>
+                <p className='text-xl font-semibold text-slate-600'>
+                  {date.locale('en').format('dddd').substring(0, 3)} {date.format('DD')}. {date.format('MM')}. {date.format('YYYY')}
+                </p>
               </div>
             </div>
-            <div className='relative p-5 flex-auto'>
+            <div className='relative p-5 flex-auto mx-10 space-y-3'>
               {events.map((event, index) => (
               <div key={index} className={`w-full h-[20px] ${event.color} rounded-md text-white font-medium text-sm mb-1 pl-1 pr-2`}>
                 <span>{`${event.title} (${event.from.format('HH:mm')} - ${event.to.format('HH:mm')})`}</span>
               </div>
               ))}
             </div>
-            <div className='flex items-center justify-center p-5 border-t-2 border-slate-200'>
+            <div className='flex items-center justify-center p-5'>
               <button
-                className='text-[18px] text-white bg-red-500 rounded-xl p-1 font-medium px-8 select-none space-x-2'
+                className='text-[18px] text-white bg-red-500 rounded-xl p-1 font-medium px-8 py-2 select-none space-x-2'
                 type='button'
                 onClick={() => setShowModal(false)}
               >
